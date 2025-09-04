d9f642303c59197632776dca6a9a541ec2bef225
AlagbeAwwal1 <awwal0421@gmail.com>
2025-09-04 20:45:19 -0300

    Feat: Added vercel front end link to the allowed origins for the backend

--- backend/monitoring/settings.py
+++ backend/monitoring/settings.py
@@ -25,6 +25,6 @@ STATIC_URL = 'static/'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    https://enterprise-monitoring-dashboard.vercel.app/
+    "https://enterprise-monitoring-dashboard.vercel.app/",
 ]
 
